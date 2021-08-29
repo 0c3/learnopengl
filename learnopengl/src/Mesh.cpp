81f666086f614f017eb4a948b391ea623c46cf94
0c3 <0ccgit@gmail.com>
2021-08-29 15:12:25 +1000

    added default constructor for mesh class, unindexed drawing option, and changed way it uses shaders

--- learnopengl/src/Mesh.cpp
+++ learnopengl/src/Mesh.cpp
@@ -6,8 +6,17 @@
 
 #include <glad/glad.h>
 
-Mesh::Mesh(vertex_t* _vertices, uint32_t _vertCount, uint32_t* _indices, uint32_t _idxCount, Texture* _textures, uint8_t _texCount)
+namespace GLDrawMode
 {
+	enum GLDrawMode {
+		Indexed = 0,
+		Arrays = 1
+	};
+}
+
+Mesh::Mesh(vertex_t* _vertices, uint32_t _vertCount, uint32_t* _indices, uint32_t _idxCount, Texture* _textures, uint8_t _texCount, Shader _shader)
+{
+	shader = _shader;
 	size_t vertSize = sizeof(vertex_t) * _vertCount;
 	size_t idxSize = sizeof(uint32_t) * _idxCount;
 
@@ -56,8 +65,9 @@ Mesh::Mesh(vertex_t* _vertices, uint32_t _vertCount, uint32_t* _indices, uint32_
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(vertex_t), (void*)offsetof(vertex_t, uv));
 }
 
-Mesh::Mesh(const char* path)
+Mesh::Mesh(const char* path, Shader _shader)
 {
+	shader = _shader;
 	FILE* fp = fopen(path, "rb");
 
 	if (!fp)
@@ -85,49 +95,88 @@ Mesh::Mesh(const char* path)
 	}*/
 
 	char line[512];
-
+	uint32_t normCount = 0;
 	for (fgets(line, 100, fp); !feof(fp); fgets(line, 100, fp))
 	{
-		if (line[0] == 'v' && line[1] == ' ')
-			vertCount++;
+		if (line[0] == 'v')
+		{
+			if (line[1] == ' ')
+				vertCount++;
+			if (line[1] == 'n')
+				normCount++;
+		}
 		if (line[0] == 'f' && line[1] == ' ')
-			idxCount += 3;
+		{
+			uint32_t x;
+			static int8_t elementsRead = sscanf(line + 2, "%u/%*u/%u %u/%*u/%u %u/%*u/%u %u/%*u/%u", &x, &x, &x, &x, &x, &x, &x, &x);		
+			idxCount += elementsRead / 2;
+		}	
 	}
 
 	size_t vertSize = sizeof(vertex_t) * vertCount;
 	vertices = (vertex_t*)malloc(vertSize);
 
+	size_t normSize = sizeof(glm::vec3) * normCount;
+	glm::vec3* normals = (glm::vec3*)malloc(normSize);
+
 	size_t idxSize = sizeof(vertex_t) * idxCount;
 	indices = (uint32_t*)malloc(idxSize);
 
-	int itVertex = 0; // vertex iterator
-	int itIndex = 0; // index iterator
+	uint32_t itVertex = 0; // vertex iterator
+	uint32_t itIndex = 0; // index iterator
+	uint32_t itNormal = 0; // normal iterator
 	rewind(fp);
 	for (fgets(line, 100, fp); !feof(fp); fgets(line, 100, fp))
 	{
-		// if this line is vertex position
-		if (line[0] == 'v' && line[1] == ' ')
+		if (line[0] == 'v')
 		{
-			glm::vec3 vertex;
-			if (sscanf(line + 2, "%f%f%f", &vertex.x, &vertex.y, &vertex.z) == 3)
+			// if this line is vertex position
+			if (line[1] == ' ')
 			{
-				vertices[itVertex].position = vertex;
-				itVertex++;
+				glm::vec3 vertex;
+				if (sscanf(line + 2, "%f%f%f", &vertex.x, &vertex.y, &vertex.z) == 3)
+				{
+					vertices[itVertex].position = vertex;
+					itVertex++;
+				}
+			}
+			// if this line is vertex normal
+			if (line[1] == 'n')
+			{
+				glm::vec3 normal;
+				if (sscanf(line + 2, "%f%f%f", &normal.x, &normal.y, &normal.z) == 3)
+				{
+					normals[itNormal] = normal;
+					itNormal++;
+				}
 			}
 		}
 		// if is face (for indices)
 		if (line[0] == 'f' && line[1] == ' ')
 		{
-			uint32_t idx[3];
-			if (sscanf(line + 2, "%u/%*u/%*u %u/%*u/%*u %u/%*u/%*u", &idx[0], &idx[1], &idx[2]) == 3)
+			uint32_t idx[4];
+			uint32_t normIdx[4];
+			int8_t elementsRead = sscanf(line + 2, "%u/%*u/%u %u/%*u/%u %u/%*u/%u %u/%*u/%u", &idx[0], &normIdx[0], &idx[1], &normIdx[1], &idx[2], &normIdx[2], &idx[3], &normIdx[3]);
+			//printf("%d\n", elementsRead);
+			if (elementsRead > 0)
 			{
-				for (uint8_t i = 0; i < 3; i++)
+				for (uint8_t i = 0; i < elementsRead / 2; i++)
 				{
-					indices[itIndex+i] = idx[i] - 1;
+					indices[itIndex + i] = idx[i] - 1;
+					glm::vec3 nigger = normals[normIdx[i] - 1];
+					vertices[indices[itIndex + i]].normal = nigger;
 				}
-				itIndex += 3;
+				itIndex += elementsRead / 2;
 			}
 		}
+	}
+
+	free(normals);
+
+	for (int i = 0; i < vertCount + 1; i++)
+	{
+		glm::vec3 x = vertices[i].normal;
+		//printf("norm %f %f %f\n", x.x, x.y, x.z);
 	}
 
 	fclose(fp);
@@ -157,6 +206,96 @@ Mesh::Mesh(const char* path)
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(vertex_t), (void*)offsetof(vertex_t, uv));
 }
 
+Mesh::Mesh(Shader* _shader, Texture* _textures, uint8_t _texCount)
+{
+	shader = *_shader;
+	vertex_t verts[] = {
+		  // positions					   // normals                      // texture coords
+		{ glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(0.0f,  0.0f, -1.0f),  glm::vec2(0.0f, 0.0f) },
+		{ glm::vec3( 0.5f, -0.5f, -0.5f), glm::vec3(0.0f,  0.0f, -1.0f),  glm::vec2(1.0f, 0.0f) },
+		{ glm::vec3( 0.5f,  0.5f, -0.5f), glm::vec3(0.0f,  0.0f, -1.0f),  glm::vec2(1.0f, 1.0f) },
+		{ glm::vec3( 0.5f,  0.5f, -0.5f), glm::vec3(0.0f,  0.0f, -1.0f),  glm::vec2(1.0f, 1.0f) },
+		{ glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec3(0.0f,  0.0f, -1.0f),  glm::vec2(0.0f, 1.0f) },
+		{ glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(0.0f,  0.0f, -1.0f),  glm::vec2(0.0f, 0.0f) },
+																		  
+		{ glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec3(0.0f,  0.0f, 1.0f),   glm::vec2(0.0f, 0.0f) },
+		{ glm::vec3( 0.5f, -0.5f,  0.5f), glm::vec3(0.0f,  0.0f, 1.0f),   glm::vec2(1.0f, 0.0f) },
+		{ glm::vec3( 0.5f,  0.5f,  0.5f), glm::vec3(0.0f,  0.0f, 1.0f),   glm::vec2(1.0f, 1.0f) },
+		{ glm::vec3( 0.5f,  0.5f,  0.5f), glm::vec3(0.0f,  0.0f, 1.0f),   glm::vec2(1.0f, 1.0f) },
+		{ glm::vec3(-0.5f,  0.5f,  0.5f), glm::vec3(0.0f,  0.0f, 1.0f),   glm::vec2(0.0f, 1.0f) },
+		{ glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec3(0.0f,  0.0f, 1.0f),   glm::vec2(0.0f, 0.0f) },
+		
+		{ glm::vec3(-0.5f,  0.5f,  0.5f), glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec2(1.0f, 0.0f) },
+		{ glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec2(1.0f, 1.0f) },
+		{ glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec2(0.0f, 1.0f) },
+		{ glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec2(0.0f, 1.0f) },
+		{ glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec2(0.0f, 0.0f) },
+		{ glm::vec3(-0.5f,  0.5f,  0.5f), glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec2(1.0f, 0.0f) },
+		
+		{ glm::vec3( 0.5f,  0.5f,  0.5f), glm::vec3(1.0f,  0.0f,  0.0f),  glm::vec2(1.0f, 0.0f) },
+		{ glm::vec3( 0.5f,  0.5f, -0.5f), glm::vec3(1.0f,  0.0f,  0.0f),  glm::vec2(1.0f, 1.0f) },
+		{ glm::vec3( 0.5f, -0.5f, -0.5f), glm::vec3(1.0f,  0.0f,  0.0f),  glm::vec2(0.0f, 1.0f) },
+		{ glm::vec3( 0.5f, -0.5f, -0.5f), glm::vec3(1.0f,  0.0f,  0.0f),  glm::vec2(0.0f, 1.0f) },
+		{ glm::vec3( 0.5f, -0.5f,  0.5f), glm::vec3(1.0f,  0.0f,  0.0f),  glm::vec2(0.0f, 0.0f) },
+		{ glm::vec3( 0.5f,  0.5f,  0.5f), glm::vec3(1.0f,  0.0f,  0.0f),  glm::vec2(1.0f, 0.0f) },
+																		  
+		{ glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(0.0f, -1.0f,  0.0f),  glm::vec2(0.0f, 1.0f) },
+		{ glm::vec3( 0.5f, -0.5f, -0.5f), glm::vec3(0.0f, -1.0f,  0.0f),  glm::vec2(1.0f, 1.0f) },
+		{ glm::vec3( 0.5f, -0.5f,  0.5f), glm::vec3(0.0f, -1.0f,  0.0f),  glm::vec2(1.0f, 0.0f) },
+		{ glm::vec3( 0.5f, -0.5f,  0.5f), glm::vec3(0.0f, -1.0f,  0.0f),  glm::vec2(1.0f, 0.0f) },
+		{ glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec3(0.0f, -1.0f,  0.0f),  glm::vec2(0.0f, 0.0f) },
+		{ glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(0.0f, -1.0f,  0.0f),  glm::vec2(0.0f, 1.0f) },
+																		  
+		{ glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec3(0.0f,  1.0f,  0.0f),  glm::vec2(0.0f, 1.0f) },
+		{ glm::vec3( 0.5f,  0.5f, -0.5f), glm::vec3(0.0f,  1.0f,  0.0f),  glm::vec2(1.0f, 1.0f) },
+		{ glm::vec3( 0.5f,  0.5f,  0.5f), glm::vec3(0.0f,  1.0f,  0.0f),  glm::vec2(1.0f, 0.0f) },
+		{ glm::vec3( 0.5f,  0.5f,  0.5f), glm::vec3(0.0f,  1.0f,  0.0f),  glm::vec2(1.0f, 0.0f) },
+		{ glm::vec3(-0.5f,  0.5f,  0.5f), glm::vec3(0.0f,  1.0f,  0.0f),  glm::vec2(0.0f, 0.0f) },
+		{ glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec3(0.0f,  1.0f,  0.0f),  glm::vec2(0.0f, 1.0f) }
+	};
+
+	vertCount = sizeof(verts) / sizeof(vertex_t);
+	vertices = (vertex_t*)malloc(sizeof(verts));
+	if (vertices)
+		memcpy(vertices, verts, sizeof(verts));
+
+	idxCount = 0;
+	indices = nullptr; // not using indexed drawing
+
+	texCount = _texCount;
+	textures = nullptr;
+	if (_texCount > 0)
+	{
+		size_t texSize = sizeof(Texture) * _texCount;
+		textures = (Texture*)malloc(texSize);
+		if (textures)
+		{
+			memcpy(textures, _textures, texSize);
+		}
+	}
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	// vertices
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), &vertices[0], GL_STATIC_DRAW);
+
+	// vertex positions
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex_t), (void*)0);
+	// vertex normals
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(vertex_t), (void*)offsetof(vertex_t, normal));
+	// vertex texture coords
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(vertex_t), (void*)offsetof(vertex_t, uv));
+
+	unindexedDrawing = true;
+}
+
 Mesh::~Mesh()
 {
 	glDeleteVertexArrays(1, &VAO);
@@ -168,7 +307,7 @@ Mesh::~Mesh()
 	free(textures);
 }
 
-void Mesh::Draw(Shader& shader)
+void Mesh::Draw()
 {
 	if (texCount > 0)
 	{
@@ -187,6 +326,13 @@ void Mesh::Draw(Shader& shader)
 		}
 	}
 	glBindVertexArray(VAO);
-	glDrawElements(GL_TRIANGLES, idxCount, GL_UNSIGNED_INT, 0);
-	//glDrawArrays(GL_TRIANGLES, 0, vertCount);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	if (unindexedDrawing)
+	{
+		glDrawArrays(GL_TRIANGLES, 0, vertCount);
+	}
+	else
+	{
+		glDrawElements(GL_TRIANGLES, idxCount, GL_UNSIGNED_INT, 0);
+	}	
 }
--- learnopengl/src/Mesh.h
+++ learnopengl/src/Mesh.h
@@ -9,18 +9,19 @@ struct vertex_t
 	glm::vec2 uv;
 };
 
-class Shader;
+#include "Shader.h"
 
 class Texture;
 
 class Mesh
 {
 public:
-	Mesh(vertex_t* _vertices, uint32_t _vertCount, uint32_t* _indices, uint32_t _idxCount, Texture* _textures, uint8_t _texCount);
-	Mesh(const char* path); // load file
+	Mesh(vertex_t* _vertices, uint32_t _vertCount, uint32_t* _indices, uint32_t _idxCount, Texture* _textures, uint8_t _texCount, Shader _shader = Shader());
+	Mesh(const char* path, Shader _shader = Shader()); // load file
+	Mesh(Shader* _shader, Texture* _textures = nullptr, uint8_t _texCount = 0); // default mesh
 	~Mesh();
 
-	void Draw(Shader& shader);
+	void Draw();
 
 public:
 	vertex_t* vertices;
@@ -33,4 +34,8 @@ public:
 	uint8_t texCount;
 
 	uint32_t VAO, VBO, EBO;
+
+	bool unindexedDrawing;
+
+	Shader shader;
 };
